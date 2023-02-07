a65c39d91a99cf9e0038466ce244090db0e31943
adiy10 <adijoshua1010@gmail.com>
2023-02-07 13:29:36 +0200

    ag-grid-enterprise packages

--- src/app/fleet-results/fleet-results.component.ts
+++ src/app/fleet-results/fleet-results.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import 'ag-grid-enterprise';
+import 'ag-grid-enterprise' ;
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
